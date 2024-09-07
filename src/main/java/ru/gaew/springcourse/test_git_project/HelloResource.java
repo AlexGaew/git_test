a2bc3d1886e0cef2a34ff27c5ffe3822dfcde326
AlexGaew <Alex_gaew@mail.ru>
2024-09-07 18:50:43 +0300

    Commit from master branch

---
+++ src/main/java/ru/gaew/springcourse/test_git_project/HelloResource.java
@@ -0,0 +1,14 @@
+package ru.gaew.springcourse.test_git_project;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
