347aadfdf30ea58b425819b60ac30c8bfa6d6f45
AlexGaew <Alex_gaew@mail.ru>
2024-09-07 19:27:20 +0300

    Commit from test_2 branch

--- src/main/java/ru/gaew/springcourse/test_git_project/Test.java
+++ src/main/java/ru/gaew/springcourse/test_git_project/Test.java
@@ -1,5 +1,7 @@
 package ru.gaew.springcourse.test_git_project;
 
+import ru.gaew.springcourse.test_git_project.test2.Test2;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -11,5 +13,7 @@ public class Test {
         System.out.println("Hello World");
         Test1 test1 = new Test1("Test1");
         test1.printName();
+        Test2 test2 = new Test2("Test2");
+        test2.printName();
     }
 }
---
+++ src/main/java/ru/gaew/springcourse/test_git_project/test2/Test2.java
@@ -0,0 +1,14 @@
+package ru.gaew.springcourse.test_git_project.test2;
+
+public class Test2 {
+    String name;
+
+    public Test2(String name) {
+        this.name = name;
+
+    }
+
+    public void printName() {
+        System.out.println(name);
+    }
+}
