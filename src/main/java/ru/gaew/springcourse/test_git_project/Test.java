d3216329e84c4f718f6b5faa02a495642d24f236
AlexGaew <Alex_gaew@mail.ru>
2024-09-07 19:20:39 +0300

    Commit from master branch

---
+++ src/main/java/ru/gaew/springcourse/test_git_project/Test.java
@@ -0,0 +1,13 @@
+package ru.gaew.springcourse.test_git_project;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.print();
+    }
+
+    void print(){
+        System.out.println("Hello World");
+    }
+}
